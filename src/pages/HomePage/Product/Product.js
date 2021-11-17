82f98750d78cc72d8d7013ed4bebbdb457ba654f
delwarJnu11 <delwar999393@gmail.com>
2021-11-17 10:26:20 +0600

    changes styles

--- src/pages/HomePage/Product/Product.js
+++ src/pages/HomePage/Product/Product.js
@@ -10,13 +10,13 @@ const Product = ({ product }) => {
     return (
         <Col>
             <Zoom>
-                <Card className="h-100 text-center">
+                <Card className="text-center">
                     <div className="card-image">
-                        <img src={image} alt="" className="img-fluid" />
+                        <Card.Img variant="top" src={image} alt="" className="img-fluid" />
                     </div>
                     <Card.Body>
                         <Card.Title className="product-title">{title}</Card.Title>
-                        <Card.Text className="product-desc">{description.slice(0, 200)}</Card.Text>
+                        <Card.Text className="product-desc">{description.slice(0, 140)}</Card.Text>
                         <h5 className="product-price">Price: ${price}</h5>
                     </Card.Body>
                     <Card.Footer className="card-footer">
