1f4387656962032471262763cab91d7a98bad670
delwarJnu11 <delwar999393@gmail.com>
2021-12-24 17:15:38 +0600

    updated

--- src/pages/HomePage/Products/Products.js
+++ src/pages/HomePage/Products/Products.js
@@ -8,31 +8,33 @@ const Products = () => {
     const { products } = useProducts();
     return (
         <>
-            <Container className="my-5">
-                <Row>
-                    <Col sm={12} md={2}></Col>
-                    <Col sm={12} md={8}>
-                        <div className="text-center">
-                            <Fade right cascade>
-                                <h3 className="featured-products">Featured Products</h3>
-                            </Fade>
-                            <Fade left cascade>
-                                <p className="featured-desc">Our product range includes Skin Whitening And Fairness Cream, Oxygen Oxygenating Mask, Skin Care Gel, Natural Hair Conditioner, Chocolate Coffee Face Wash</p>
-                            </Fade>
-                        </div>
-                    </Col>
-                    <Col sm={12} md={2}></Col>
-                </Row>
-                {
-                    !products.length ? <div className="text-center">
-                        <Spinner variant="success" animation="border"></Spinner>
-                    </div> : <Row xs={1} md={3} className="g-4 mt-4">
+            {products.length === 0 ? <div className="text-center">
+                <Spinner variant="success" animation="border"></Spinner>
+            </div> :
+                <Container className="my-5">
+                    <Row>
+                        <Col sm={12} md={2}></Col>
+                        <Col sm={12} md={8}>
+                            <div className="text-center">
+                                <Fade right cascade>
+                                    <h3 className="featured-products">Featured Products</h3>
+                                </Fade>
+                                <Fade left cascade>
+                                    <p className="featured-desc">Our product range includes Skin Whitening And Fairness Cream, Oxygen Oxygenating Mask, Skin Care Gel, Natural Hair Conditioner, Chocolate Coffee Face Wash</p>
+                                </Fade>
+                            </div>
+                        </Col>
+                        <Col sm={12} md={2}></Col>
+                    </Row>
+
+                    <Row xs={1} md={3} className="g-4 mt-4">
                         {
                             products.slice(0, 6).map(product => <Product key={product._id} product={product}></Product>)
                         }
                     </Row>
-                }
-            </Container>
+
+                </Container>
+            }
         </>
     );
 };
